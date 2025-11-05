6ef1880fe0d09d2610d1d719b8ea9c10f6ea5633
vanyabhasinn <bhasinvanya90@gmail.com>
2025-11-05 17:32:23 +0530

    Create ApigatewayApplication.java

---
+++ apigateway/ApigatewayApplication.java
@@ -0,0 +1,18 @@
+package com.ncu.library.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages="com.ncu.library.apigateway")
+public class ApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayApplication.class, args);
+	}
+
+}
