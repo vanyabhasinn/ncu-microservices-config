2a114230ed2a33c7f924d33cc2f0a053094e5f57
vanyabhasinn <bhasinvanya90@gmail.com>
2025-11-05 17:50:40 +0530

    Create ApplicationConfig.java

---
+++ borrowservice/config/ApplicationConfig.java
@@ -0,0 +1,26 @@
+package com.ncu.library.borrowservice.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    // CHANGED: Add RestTemplate bean, required for Eureka/service name resolution!
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
+
+
+
