b95a294093cb669785198c40374ad77eed8f7b42
vanyabhasinn <bhasinvanya90@gmail.com>
2025-11-05 18:24:08 +0530

    Create UserserviceApplication.java

---
+++ userservice/UserserviceApplication.java
@@ -0,0 +1,17 @@
+package com.ncu.library.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages="com.ncu.library.userservice")
+@EnableDiscoveryClient
+public class UserserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserserviceApplication.class, args);
+	}
+
+}
