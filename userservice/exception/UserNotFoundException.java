8b3a9e2ffbfe98b7882ecf0fbd6c79d49fe65aa7
vanyabhasinn <bhasinvanya90@gmail.com>
2025-11-05 18:20:04 +0530

    Create UserNotFoundException.java

---
+++ userservice/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.ncu.library.userservice.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
